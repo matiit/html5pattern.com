f39535e3b50f0f32c8d13494cc73153ead1c0761
Aurel Hermand <github@devone.de>
2014-07-20 00:05:59 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>HTML5Pattern</title>
+    <base href="http://html5pattern.com/" />
+    <link rel="stylesheet" type="text/css" href="html5pattern/css/normalize.css" />
+    <link rel="stylesheet" type="text/css" href="html5pattern/css/style.css" />
+  </head>
+  <body>
+  
+  
+  
+  </body>
+</html>
